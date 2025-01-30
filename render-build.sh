a2de73e32c9e95b734c95f58e5b2077f6edd9bf1
Sreb0 <srebpetrov@gmail.com>
2025-01-30 17:17:33 +0200

    render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# Install dependencies
+npm install
+
+# Ensure the Puppeteer cache directory exists
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
+
+# Install Puppeteer and download Chrome
+npx puppeteer browsers install chrome
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+    echo "...Copying Puppeteer Cache from Build Cache"
+    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+else
+    echo "...Storing Puppeteer Cache in Build Cache"
+fi
--- src/services/detectM3U8File.js
+++ src/services/detectM3U8File.js
@@ -1,12 +1,10 @@
 const puppeteer = require('puppeteer');
-process.env.PUPPETEER_CACHE_DIR || '/opt/render/.cache/puppeteer';
 
 
 // Function to launch Puppeteer, set up network response listener, and navigate to the given URL
 async function detectM3U8File(url) {
     const browser = await puppeteer.launch({
         headless: true,
-        executablePath:  process.env.CHROME_BIN ||'/usr/bin/google-chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
